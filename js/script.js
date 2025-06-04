2c104dcf605520d5309fe8289f2cd8cea2cd32cf
ItsHoward <96213776+weeaboo69@users.noreply.github.com>
2025-06-04 12:46:40 +0800

    0604-1

--- js/script.js
+++ js/script.js
@@ -75,22 +75,40 @@ monogatari.assets('scenes', {
 
 // Define the Characters
 monogatari.characters({
-	'manA': {
-		name: '阿德',
+	'char_a': {
+		name: '鄭筱臻',
 		color: '#008000',
 		sprites: {
-			face01: 'man01.png',
-			face02: 'man02.png',
-			face03: 'man03.png',
+			angry:'1.jpg',
+			sad:'1.jpg',
+			happy:'1.jpg'
 		}
 	},
-	'manB': {
-		name: '安安',
+	'char_b': {
+		name: '鄭學靖',
 		color: '#00ff88',
 		sprites: {
-			face01: 'woman01.png',
-			face02: 'woman02.png',
-			face03: 'woman03.png',
+			angry:'2.jpg',
+			sad:'2.jpg',
+			happy:'2.jpg'
+		}
+	},
+	'char_c': {
+		name: '朱曉雯',
+		color: '#006400',
+		sprites: {
+			angry:'5.jpg',
+			sad:'3.jpg',
+			happy:'3.jpg'
+		}
+	},
+	'char_d': {
+		name: '鄭治忠',
+		color: '#00BFFF',
+		sprites: {
+			angry:'4.jpg',
+			sad:'4.jpg',
+			happy:'4.jpg'
 		}
 	}
 });
@@ -99,28 +117,47 @@ monogatari.script({
 	// The game starts here.
 	'Start': [
 		'show scene #f7f6f6 with fadeIn',
-		'show character manA face01',
-		'manA 嗨我是大手阿德',
-		'show character manA face02 at left',
-		'manA 我好帥',
-		'show character manB face02 at right',
+		'char_d 人氣女子樂團Ave Mujica 在重新復活之後要舉辦很多場演唱會。',
+		'char_d 為此團員做了很多新歌要在演唱會上演奏。',
+		'char_d 但是樂團的吉他手小睦沒辦法演奏那麼高難度的歌曲。',
+		'char_d 為了不讓演唱會延期團員們決定在演唱會用預錄的吉他聲音並讓吉他手在台上假裝演奏。',
+		'char_d 但演唱會當天因為器材問題被觀眾發現了。',
+		'show character char_c sad at left with fadeIn',
+		'char_c 剛剛的演唱會...真的太糟糕了。',
+		'show character char_b sad at right with fadeIn',
+		'char_b 對不起...都是我的錯。',
+		'char_b 完全忘記了怎麼彈吉他...',
+		'hide character char_c',
+		'show character char_c angry at left with fadeIn',
+		'char_c 你知道這對我們樂團的名聲造成多大的傷害嗎？',
+		'char_c 觀眾們現在都在網上討論我們是『假樂團』！',
+		'char_b 我...我已經練習了很久，但是還是找不回那種感覺。',
+		'char_b 也許我真的不適合當吉他手...',
+		'char_c 現在說這些有什麼用？損害已經造成了！',
+		'char_c 我們應該公開道歉，然後暫停活動，等風頭過了再重新開始。',
+		'show character char_a sad with fadeIn',
+		'char_a 冷靜一點，祥子。我們現在需要想辦法解決問題，而不是互相指責。',
+		'char_d 小睦拿起東西準備離開，這時初華注意到她的手在微微顫抖...',
+		'char_d 這時候初華選擇了...',
+		{
+			'Choice':{
+				fork_a:{
+					'Text':'支持小睦',
+					'Do':'jump fork_a'
+				},
+				fork_b:{
+					'Text':'同意祥子的建議',
+					'Do':'jump fork_b'
+				}
+			}
+		}
 	],
 
-	'Yes': [
-		'y Thats awesome!',
-		'y Then you are ready to go ahead and create an amazing Game!',
-		'y I can’t wait to see what story you’ll tell!',
-		'end'
+	'fork_a': [
+		'char_a zxw'
 	],
 
-	'No': [
-
-		'y You can do it now.',
-
-		'show message Help',
-
-		'y Go ahead and create an amazing Game!',
-		'y I can’t wait to see what story you’ll tell!',
-		'end'
+	'fork_b': [
+		'char_a zzz'
 	]
 });
